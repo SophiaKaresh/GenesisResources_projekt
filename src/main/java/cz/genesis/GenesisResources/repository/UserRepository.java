1501cb2c2eba5f9baf9b52fd8d7537c3dcd15b94
SophiaKaresh <karesovasophia@seznam.cz>
2026-02-19 18:58:57 +0100

    Extend UserRepository to include findByPersonid method

--- src/main/java/cz/genesis/GenesisResources/repository/UserRepository.java
+++ src/main/java/cz/genesis/GenesisResources/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package cz.genesis.GenesisResources.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import cz.genesis.GenesisResources.model.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByPersonid(String personid);
 }
